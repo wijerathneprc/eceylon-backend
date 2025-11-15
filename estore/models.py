7e27ac355eb723ff71ccf0876f99cf2a101ce30f
Rishan Wijerathne <wijerathneprc92@gmail.com>
2025-11-15 13:55:08 +0530

    start developing estore feature

---
+++ estore/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description= models.TextField()
+    base_price = models.DecimalField(max_digits=6, decimal_places=2)
+    date_created = models.DateTimeField(auto_now=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
