614ee74c32a0d3ecf24894b026d67cea2b2920ac
Rishan Wijerathne <wijerathneprc92@gmail.com>
2025-12-10 15:08:46 +0530

    Modify estore to represent general product info
    update Product and ProductModel classes to hold general details for any product and its model

--- estore/models.py
+++ estore/models.py
@@ -1,73 +1,83 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Brand(models.Model):
+    """ Represents a brand in the online store. """ 
     name = models.CharField(max_length=255)
 
     def __str__(self):
+        """ display brand name for string representation"""
         return self.name
+
 
 class Category(models.Model):
+    """ Represents a product category in the online store. """
     name = models.CharField(max_length=255)
 
     def __str__(self):
+        """ display category name for string representation"""
         return self.name
 
+
 class Product(models.Model):
+    """
+    Represents a product available for sale in the online store.
+
+    Each product has a name, description, brand, and category.
+    These are general details for any product which do not depend on the product category or model.
+    """
     name = models.CharField(max_length=255)
-    description = models.TextField(null=True)
+    description = models.TextField()
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
     def __str__(self):
+        """ display product name for string representation"""
         return self.name
+  
     
 class Image(models.Model):
-    product = models.ForeignKey(Product,related_name='image', on_delete=models.CASCADE)
-    color = models.CharField(max_length=255,default='any color' )
-    image = models.ImageField(upload_to='images/product', blank=True, null=True)
+    """
+    Represents an image associated with a specific product.
     
-
-      
-class ProductConfig(models.Model):
-    product = models.ForeignKey(Product,related_name='config', on_delete=models.CASCADE)
+    Each image is linked to a specific product and includes a color description.
+    """
+    product = models.ForeignKey(Product, related_name='image', on_delete=models.CASCADE)
+    color = models.CharField(max_length=255,default='any color' )
+    image = models.ImageField(upload_to='images/product')
+    
+    def __str__(self):
+        """ display product name and color name for string representation"""
+        return f"{self.product.name} - {self.color}"
+    
+     
+class ProductModel(models.Model):
+    """
+    Represents a specific model or variant of a product with general details that does not depend on the product category.
+    
+    Each product model is linked to a general product and may have an associated image.
+    each model has its own pricing and stock details.
+    """
+    product = models.ForeignKey(Product, related_name='product-model', on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, related_name='product-model-image' on_delete=models.SET_NULL, null=True)
     model_name = models.CharField(max_length=255)
-    other_name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    other_name = models.CharField(max_length=255)  
     release_date = models.DateField()
-    image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True)
-    stock = models.IntegerField()
-    sold_count = models.IntegerField(default=0)
-    warranty_period = models.IntegerField()  # in months
-    is_active = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available_quantity = models.IntegerField()
+    sold_quantity = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     
+    def is_in_stock(self):
+        """
+        Checks if the product model is currently in stock.
 
-class UserAddress(models.Model):
-    PROVINCE_CHOICES = [
-        ('CP', 'Central Province'),('EP', 'Eastern Province'),('NCP', 'North Central Province'), ('NP', 'Northern Province'),('NWP', 'North Western Province'),('SMP', 'Sabaragamuwa Province'),('SP', ' Southern Province'),('UP', ' Uva Province'),('WP', 'Western Province')
-    ]
-    DISTRICT_CHOICES =[
-        ('1','Jaffna'),('2','Kilinochchi'),('3','Mannar'),('4','Mullaitivu'),('5','Vavuniya'),('6','Puttalam'),('7','Kurunegala'),('8','Gampaha'),('9','Colombo'),('10','Kalutara'),('11','Trincomalee'),('12','Batticaloa'), ('13','Ampara'),('14','Badulla'), ('15','Monaragala'),('16','Hambantota'),('17','Matara'),('18','Galle'),('19','Anuradhapura'),('20','Polonnaruwa'),('21','Matale'),('22','Kandy'),('23','Nuwara Eliya'),('24','Kegalle'),('25','Ratnapura')
-    ]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    province = models.CharField(max_length=3, choices=PROVINCE_CHOICES, default='CP')
-    district = models.CharField(max_length=2, choices=DISTRICT_CHOICES, default='1')
-    city = models.CharField(max_length=255)
-    street_line_01 = models.CharField(max_length=255)
-    street_line_02 = models.CharField(max_length=255)
-    postal_code = models.IntegerField()
-    default=models.BooleanField(default=False)
+        Returns:
+            bool: True if available_quantity is greater than 0, False otherwise.
+        """
+        return self.available_quantity > 0
     
-    
-class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_config_id = models.ForeignKey(ProductConfig, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    selected = models.BooleanField(default=True)
-    
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_config = models.ForeignKey(ProductConfig, on_delete=models.CASCADE)
-    delvery_state = models.CharField(max_length=2)
-    delivery_estimation = models.DateField()
+    def __str__(self):
+        """ display product name and model name for string representation"""
+        return f"{self.product.name} - {self.model_name}"
